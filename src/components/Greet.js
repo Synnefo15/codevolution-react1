8bd226621bda22342b05a26ae99017bf927f8b71
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-05-18 23:57:11 +0700

    vid6

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// &---- Functional ----
+// function Greet() {
+//     return <h1>Hallo Fungsi Rafi</h1>
+// }
+
+// &---- Const func ----
+const Greet = () => {
+    return <h1>Hallo Class rafi</h1>
+}
+
+export default Greet
---
+++ src/components/Welcome.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+class Welcome extends Component{
+    render(){
+        return <h3>Class Component</h3>
+    }
+
+}
+
+export default Welcome
